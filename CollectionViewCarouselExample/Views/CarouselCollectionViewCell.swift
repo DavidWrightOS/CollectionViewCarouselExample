1254c7056b07acb4306ffdd0f52f3a03dd6566f1
David Wright <davidtwright89@gmail.com>
2020-05-09 18:11:50 -0400

    Initial Commit + Basic setup of file structure and storyboard

---
+++ CollectionViewCarouselExample/Views/CarouselCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CarouselCollectionViewCell.swift
+//  CollectionViewCarouselExample
+//
+//  Created by David Wright on 5/9/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class CarouselCollectionViewCell: UICollectionViewCell {
+    
+}
